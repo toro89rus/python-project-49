686c1763c6915c26c139bc5b4df350511dbe93d9
toro <timofey89rus@gmail.com>
2024-08-08 19:03:02 +0300

    fix game_engine, add game_rule to print before loop, delete unused code

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,13 +1,13 @@
 import prompt
-from random import randint
 
 
-def game_engine(game_type):
+def game_engine(game_rule, game_type):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     MAX_ROUNDS = 3
     correct_answers = 0
+    print(game_rule)
     while correct_answers < MAX_ROUNDS:
         game_question, game_answer = game_type()
         print(f'Question: {game_question}')
@@ -22,18 +22,3 @@ def game_engine(game_type):
             break
     if correct_answers == 3:
         print(f'Congratulations, {name}!')
-
-
-def brain_even():
-    MIN_NUMBER = 1
-    MAX_NUMBER = 100
-    number_to_quess = randint(MIN_NUMBER, MAX_NUMBER)
-    if is_even(number_to_quess):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    return number_to_quess, correct_answer
-
-
-def is_even(number):
-    return number % 2 == 0
